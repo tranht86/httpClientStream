3c89d2a85970ec30105f007976f7c11a3172514a
Henry T <tran_ht@hotmail.com>
2017-04-10 02:54:49 +0000

    Step 5

--- https_example.js
+++ https_example.js
@@ -8,8 +8,9 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
@@ -17,3 +18,4 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
